6cfec8ba0f152773b132588c9c30b86669a9e184
krystian <krysti4nape@gmail.com>
2024-05-28 16:28:04 +0200

    added processed paths

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 
+# Define the path to the data directory
 data = Path('data')
+
+PROCESSED_TRAIN_DATA = data / 'processed' / 'train.csv'
+PROCESSED_TEST_DATA = data / 'processed' / 'test.csv'
+
 RAW_TRAIN_DATA = data / 'raw' / 'train.csv'
 RAW_TEST_DATA = data / 'raw' / 'test.csv'
