241e7bb2da9154cde2507c4456d44a5b029fa375
krystian <krysti4nape@gmail.com>
2024-06-06 00:53:22 +0200

    added notebook - model experiments

--- src/__init__.py
+++ src/__init__.py
@@ -3,8 +3,7 @@ from pathlib import Path
 # Define the path to the data directory
 data = Path('data')
 
-PROCESSED_TRAIN_DATA = data / 'processed' / 'train.csv'
-PROCESSED_TEST_DATA = data / 'processed' / 'test.csv'
+TRAIN_DATA_PATH = data / 'train.csv'
+TEST_DATA_PATH = data / 'test.csv'
 
-RAW_TRAIN_DATA = data / 'raw' / 'train.csv'
-RAW_TEST_DATA = data / 'raw' / 'test.csv'
+CACHE_DIR = 'cache'
